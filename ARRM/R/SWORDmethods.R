179fc7cf45b1a3f78b755693eb359813525a2214
Karina Nielsen <karni@space.dtu.dk>
2024-09-23 16:23:51 +0200

    SWORD reach order fixed

--- ARRM/R/SWORDmethods.R
+++ ARRM/R/SWORDmethods.R
@@ -4,7 +4,7 @@ testCon<-function(tb,ta){
     sqrt((tb$x-ta$x)^2+(tb$y-ta$y)^2)
 }
 
-reOrder<-function(out){
+reOrder2<-function(out){
     tt<-split(out,out$reachID)
     tb<-as.numeric(unlist(lapply(tt,nrow)))    
     ta<-rep(1,length(tt))
@@ -16,6 +16,159 @@ reOrder<-function(out){
     out<-do.call(rbind,tt)
     out
 }
+
+
+
+
+
+reOrder<-function(tt,mytype){
+    new<-tt
+    if(mytype=='e'){
+        new<-tt[nrow(tt):1,]
+    }
+    new
+
+}
+
+
+
+ll2UTM<-function(CLinfo){
+    LL<-cbind(CLinfo$x,CLinfo$y)
+    myProj<-getProjUTM(LL[,1], LL[,2])
+    v <- vect(LL, crs="+proj=longlat")
+    u <- terra::project(v, myProj)
+    xydat <- crds(u)
+    xx<-xydat[,1]
+    yy<-xydat[,2]
+    cbind(CLinfo,xx,yy)
+}
+
+
+fixCL<-function(CL,lim=300){
+
+    out1<-ll2UTM(CL)
+    tt <- split(out1, out1$reachID)
+    te <- lapply(tt, getEnd)
+    te <-do.call(rbind,te)
+    ts <- lapply(tt, getStart)
+    ts<-do.call(rbind,ts)
+    nr<-length(tt)
+    #find end line segment
+    firstID<-lapply(1:nrow(te),function(i)findFirst(te[i,],te,ts))
+    firstID<-do.call(rbind,firstID)
+    idf<-which(firstID$de > lim | firstID$ds > lim)[1]
+
+
+    # find next line segment
+    new<-list()
+    myend<-findEnd(te[idf,],te,ts)
+    new[[1]]<-tt[[idf]]
+    if(myend$dist > lim){
+        myend<-findEnd(ts[idf,],te,ts)
+        new[[1]]<-reOrder(tt[[idf]],'e')      
+    }
+    
+
+    nn<-list()
+    nn[[1]]<-myend
+    for(i in 2:(nr-1)){
+        myend<-getNext(myend,te,ts)
+        nn[[i]]<-myend
+    }
+    nn<-do.call(rbind,nn)
+    
+
+    
+                                        #evt reorder
+    id<-nn$id
+    for (i in 1:(nr-1)){
+        new[[i+1]]<-reOrder(tt[[id[i]]],nn$type[i])      
+    }
+
+    
+    new<-do.call(rbind,new)
+    if(new$wse[1]> new$wse[nrow(new)]) new<-new[nrow(new):1,]
+    new
+}
+
+findEnd<-function(thiste,te,ts){
+    id<-which(te$reachID==thiste$reachID)
+    te2<-te[-id,]
+    ts2<-ts[-id,]
+
+    ss<-sqrt((thiste$x - ts$x[-id])^2 + (thiste$y - ts$y[-id])^2)
+    ee<-sqrt((thiste$x - te$x[-id])^2 + (thiste$y - te$y[-id])^2)
+    is<-which.min(ss)
+    ie<-which.min(ee)
+
+    if(ss[is] < ee[ie]){
+        ids<-which(ts$reachID==ts2[is,1])
+        yy<-data.frame(reachid=thiste[1],mymatch=ts2[is,1],id=ids,dist=ss[is],type='s')
+    }else{
+        ide<-which(te$reachID==te2[ie,1])
+        yy<-data.frame(reachid=thiste[1],mymatch=te2[ie,1],id=ide,dist=ee[ie],type='e')
+    }
+yy
+
+}
+
+
+findFirst<-function(thiste,te,ts){
+    id<-which(te$reachID==thiste$reachID)
+    thists<-ts[id,]
+    te2<-te[-id,]
+    ts2<-ts[-id,]
+
+    ss<-sqrt((thiste$x - ts$x[-id])^2 + (thiste$y - ts$y[-id])^2)
+    ee<-sqrt((thiste$x - te$x[-id])^2 + (thiste$y - te$y[-id])^2)
+    me<-c(ss,ee)
+    ime<-which.min(me)
+
+
+    ss<-sqrt((thists$x - ts$x[-id])^2 + (thists$y - ts$y[-id])^2)
+    ee<-sqrt((thists$x - te$x[-id])^2 + (thists$y - te$y[-id])^2)
+    ms<-c(ss,ee)
+    ims<-which.min(ms)
+    out<-data.frame(reachid=thiste[1],de=me[ime],ds=ms[ims])
+
+    out
+
+}
+
+
+
+
+
+getNext<-function(myend,te,ts){
+    
+    if(myend$type=='e'){
+        ar1<-ts
+    }else{
+        ar1<-te
+    }
+    id<-myend$id
+    myend<-findEnd(ar1[id,],te,ts)
+    myend
+}
+
+
+
+
+getEnd<-function(tt){
+    nr<-nrow(tt)
+    out<-data.frame(reachID=tt$reachID[nr],x=tt$xx[nr],y=tt$yy[nr])
+    out
+}
+
+getStart<-function(tt){
+    out<-data.frame(reachID=tt$reachID[1],x=tt$xx[1],y=tt$yy[1])
+    out
+}
+
+
+
+
+
 
 
 
@@ -59,7 +212,7 @@ getReachInfo<-function(thisreach,path2sword,SWORDv){
         rivername<-file[["nodes/river_name"]][id]
         dist<-file[["nodes/dist_out"]][id]
 
-        out<-data.frame(reachID=reach_id,x=x,y=y,nodeID=node_id,dist=dist,name=rivername)
+        out<-data.frame(reachID=reach_id,x=x,y=y,nodeID=node_id,dist=dist,name=rivername,wse=wse)
     }
     h5close(file)
     out
@@ -116,9 +269,10 @@ extractCL<-function(myreaches,path2sword,SWORDv){
     o<-order(myreaches)
     out<-lapply(1:length(myreaches),function(i)getReachInfo(myreaches[o[i]],path2sword,SWORDv))
     out<-do.call(rbind,out)
-    o<-order(out$dist)
-    out<-out[o,]
-    out<-reOrder(out)
+    out<-fixCL(out,lim=300)
+    #o<-order(out$dist)
+    #out<-out[o,]
+    #out<-reOrder(out)
     out
 }
 
